5ee964b12cf46ef70ee21d3588b642c047bae2d4
slimetsp <30321316+slimetsp@users.noreply.github.com>
2021-08-09 09:19:16 +0200

    remove more stuff

--- src/extension.ts
+++ src/extension.ts
@@ -35,73 +35,6 @@ class SwaggerTreeProvider
     }
 }
 
-interface BodyParam {
-    name: string;
-    type: string;
-}
-
-interface HttpMethod {
-    type: string;
-    requestBody: BodyParam[];
-}
-
-class Endpoint extends vscode.TreeItem {
-    constructor(public readonly name: string, public readonly methods: HttpMethod[]) {
-        super(name, vscode.TreeItemCollapsibleState.Collapsed);
-        this.description = methods.map((method) => method.type).join(", ");
-        this.iconPath = new vscode.ThemeIcon("symbol-interface");
-    }
-}
-
-class HttpMethodTreeItem extends vscode.TreeItem {
-    constructor(public readonly method: HttpMethod) {
-        super(
-            method.type,
-            method.requestBody.length > 0
-                ? vscode.TreeItemCollapsibleState.Collapsed
-                : vscode.TreeItemCollapsibleState.None
-        );
-        this.description = "Endpoint";
-        this.contextValue = "httpmethod";
-
-        switch (method.type) {
-            case "get":
-                this.iconPath = new vscode.ThemeIcon("cloud-download");
-                break;
-
-            case "delete":
-                this.iconPath = new vscode.ThemeIcon(
-                    "trash",
-                    new vscode.ThemeColor("testing.iconFailed")
-                );
-                break;
-
-            case "post":
-                this.iconPath = new vscode.ThemeIcon(
-                    "cloud-upload",
-                    new vscode.ThemeColor("testing.iconPassed")
-                );
-                break;
-
-            case "put":
-                this.iconPath = new vscode.ThemeIcon(
-                    "debug-step-into",
-                    new vscode.ThemeColor("foreground")
-                );
-
-                break;
-
-            case "patch":
-                this.iconPath = new vscode.ThemeIcon("wrench");
-                break;
-
-            default:
-                this.iconPath = new vscode.ThemeIcon("debug-breakpoint-unverified");
-                break;
-        }
-    }
-}
-
 class RequestBodyParameter extends vscode.TreeItem {
     constructor(
         public readonly parameter: {
@@ -118,9 +51,8 @@ class RequestBodyParameter extends vscode.TreeItem {
 export function activate(context: vscode.ExtensionContext) {
     const defaultSwaggerJsonUrl = "http://localhost:5000/swagger/v1/swagger.json";
     const swaggerTree = new SwaggerTreeProvider(defaultSwaggerJsonUrl);
-    const tree = vscode.window.registerTreeDataProvider<
-        HttpMethodTreeItem | RequestBodyParameter | Endpoint
-    >("swagger", swaggerTree);
+
+    const tree = vscode.window.registerTreeDataProvider("swagger", swaggerTree);
 
     const setSwaggerJsonUrlCommand = async () => {
         const newUrl = await vscode.window.showInputBox({
